7211aacf00c12b9f74d1f42885844756583597cd
LmuBroadcastApp <lmu.broadcast.app@proton.me>
2026-03-20 20:07:33 +0100

    Update driver panel positions gain/lost

--- overlays/styles/js/panels/driver.js
+++ overlays/styles/js/panels/driver.js
@@ -19,11 +19,17 @@ class DriverPanel
         }
 
         let diff_pos = this.vehicle.race_position_class - this.vehicle.qualy_position_class;
-        let diff_pos_txt = diff_pos > 0 ? "↓ " + Math.abs(diff_pos) : "↑ " + Math.abs(diff_pos);
+        let diff_pos_txt = "";
+
+        if (this.vehicle.qualy_position_class > 0)
+        {
+            diff_pos_txt = diff_pos > 0 ? "⮟ " + Math.abs(diff_pos) : "⮝ " + Math.abs(diff_pos);
+            diff_pos_txt += "&nbsp;&nbsp;&nbsp;Q" + this.vehicle.qualy_position_class;
+        }
 
         $(this.element_id + " .driver-panel-name").text(this.vehicle.driver);
         $(this.element_id + " .driver-panel-team").text(this.vehicle.vehicle_name);
-        $(this.element_id + " .driver-panel-pos-diff").text(diff_pos_txt);
+        $(this.element_id + " .driver-panel-pos-diff").html(diff_pos_txt);
 
         $(this.element_id + " .driver-panel-last-lap").text(LaptimeToString(this.vehicle.last_lap));
         $(this.element_id + " .driver-panel-best-lap").text(LaptimeToString(this.vehicle.best_lap));
