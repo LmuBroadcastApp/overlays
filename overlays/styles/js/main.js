f5f7f9faa31fc9f31d727a341642f4639de07691
LmuBroadcastApp <lmu.broadcast.app@proton.me>
2026-04-17 16:46:23 +0200

    Modify ramaining root CSS section from the app

--- overlays/styles/js/main.js
+++ overlays/styles/js/main.js
@@ -37,20 +37,49 @@ function UpdateOverlaySettings(settings)
     root.style.setProperty('--standings-panel-position-left', settings.standings.position_left);
     root.style.setProperty('--standings-panel-position-top', settings.standings.position_top);
 
+    root.style.setProperty('--standings-panel-font-weight', settings.standings.font_weight);
+    root.style.setProperty('--standings-panel-font-size', settings.standings.font_size);
+
+    root.style.setProperty('--standings-panel-primary-color', settings.standings.primary_color);
+    root.style.setProperty('--standings-panel-secondary-color', settings.standings.secondary_color);
+
+    root.style.setProperty('--standings-panel-gap-less-1-color', settings.standings.gap_less_1_color);
+    root.style.setProperty('--standings-panel-gap-less-2-color', settings.standings.gap_less_2_color);
+
     // session panel
     root.style.setProperty('--session-panel-position-top', settings.session.position_top);
+    root.style.setProperty('--session-panel-font-size', settings.session.font_size);
 
     // driver panel
     root.style.setProperty('--driver-panel-position-bottom', settings.driver.position_bottom);
     root.style.setProperty('--driver-panel-position-right', settings.driver.position_right);
 
+    root.style.setProperty('--driver-panel-background-color', settings.driver.background_color);
+    root.style.setProperty('--driver-panel-font-size', settings.driver.font_size);
+    root.style.setProperty('--driver-panel-width', settings.driver.width);
+
     // weather panel
     root.style.setProperty('--weather-panel-position-right', settings.weather.position_right);
     root.style.setProperty('--weather-panel-position-top', settings.weather.position_top);
 
+    root.style.setProperty('--weather-panel-img-width', settings.weather.image_width);
+    root.style.setProperty('--weather-panel-font-size', settings.weather.font_size);
+
+    root.style.setProperty('--weather-panel-header-background-color', settings.weather.header_background_color);
+    root.style.setProperty('--weather-panel-body-background-color', settings.weather.body_background_color);
+    root.style.setProperty('--weather-panel-text-color', settings.weather.text_color);
+
     // map panel
     root.style.setProperty('--map-panel-position-right', settings.map.position_right);
     root.style.setProperty('--map-panel-position-top', settings.map.position_top);
+
+    // replay banner
+    root.style.setProperty('--replay-banner-position-left', settings.replay.position_left);
+    root.style.setProperty('--replay-banner-position-top', settings.replay.position_top);
+
+
+    // telemetry
+    root.style.setProperty('--telemetry-gauge-size', settings.telemetry.gauge_size);
 }
 
 const callBacks =
